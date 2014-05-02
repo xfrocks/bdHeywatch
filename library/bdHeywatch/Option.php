c461fa5423f8e4b85f9c4462c5644d0b65d59edd
Dao Hoang Son <daohoangson@gmail.com>
2014-05-03 02:30:28 +0700

    fixed bug with space in output formats option

--- library/bdHeywatch/Option.php
+++ library/bdHeywatch/Option.php
@@ -11,7 +11,7 @@ class bdHeywatch_Option
 			case 'inputExtensions':
 				return preg_split('/\s+/', utf8_strtolower($options->bdHeywatch_inputExtensions), NULL, PREG_SPLIT_NO_EMPTY);
 			case 'outputFormats':
-				return preg_split('/\s+/', utf8_strtolower($options->bdHeywatch_outputFormats), NULL, PREG_SPLIT_NO_EMPTY);
+				return preg_split('/\n/', utf8_strtolower($options->bdHeywatch_outputFormats), NULL, PREG_SPLIT_NO_EMPTY);
 		}
 
 		return $options->get(sprintf('bdHeywatch_%s', $key), $subKey);
